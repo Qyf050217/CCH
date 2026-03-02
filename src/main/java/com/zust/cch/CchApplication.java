5cd9d2c86425f4c8fdd46bcd9213d5695b3aa8dd
qyf050217 <2500492942@qq.com>
2026-03-02 18:08:09 +0800

    初始化

---
+++ src/main/java/com/zust/cch/CchApplication.java
@@ -0,0 +1,13 @@
+package com.zust.cch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CchApplication.class, args);
+    }
+
+}
